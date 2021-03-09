e23b6063cb78b7c2b5d0873e3e8e01f67db5e822
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-09 11:20:41 -0800

    using Head to make custom title and meta data for each page to make more SEO friendly

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+export default function About() {
+  return (
+    <div>
+        <Head>
+        <title>About Page</title>
+        <meta name="keywords" content="web development, programming"/>
+        </Head>
+      <h1>About</h1>
+    </div>
+  )
+}
